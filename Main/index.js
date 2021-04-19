00c0692093ae6efc984c877045633e7b56f5acc9
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-04-19 21:23:56 +1000

    events :flushed:

--- Main/index.js
+++ Main/index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client() 
 const config = require('./config.json')
+const embedupdatemessage = new Discord.MessageEmbed
+const { LogsChannel } = require('./config.json')
 
 client.commands = new Map()
 client.aliases = new Map()
@@ -18,6 +20,42 @@ client.on('ready', async () => {
         client.login(config.token)
       }, 3600000); 
 })
+
+client.on("messageUpdate", function(oldMessage, newMessage){
+  embedupdatemessage.setTitle('**Message Changed!**')
+  embedupdatemessage.setFooter('Hob Bot')
+  embedupdatemessage.setColor('#00AAFF')
+
+  embedupdatemessage.setAuthor('Hob Bot Moderation Log')
+  
+  embedupdatemessage.addFields(
+      {
+      name: `New Message`,
+      value: `${newMessage}`,
+      inline: true
+      },
+  
+      {
+          name: `Old Message`,
+          value: `${oldMessage}`,
+          inline: true
+      },
+
+      {
+        name: `Author`,
+        value: `${newMessage.author}`,
+        inline: true
+      },
+
+      {
+        name: `Channel`,
+        value: `${newMessage.channel}`,
+        inline: true
+      }
+  )
+
+  client.channels.cache.get(`${LogsChannel}`).send(embedupdatemessage)
+});
 
 client.login(config.token)
 
